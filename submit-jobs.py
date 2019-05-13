3aedfe0c5035bc53f983a2f8f53d05c8b14e8921
Deepak Kar <kar.deepak2011@gmail.com>
2019-05-13 12:32:48 +0000

    Change

--- submit-jobs.py
+++ submit-jobs.py
@@ -11,15 +11,17 @@ def submit_jobs():
 	CONFIG_KIND = "config"
 	client = datastore.Client()
 	query = client.query(kind=CONFIG_KIND)
+	query.add_filter("outputFolder", "=", "AMDS/")
 	entities = list(query.fetch())
 
 	counter = min([ent["counter"] for ent in entities if ent["counter"] > 1])
 	running = len([ent for ent in entities if ent["running"] and ent["counter"] > 1])
-	
+
 	query = client.query(kind=CONFIG_KIND)
 	query.add_filter("counter", "=", counter)
+	query.add_filter("outputFolder", "=", "AMDS/")
 	entities = list(query.fetch())
-		
+
 	while running < 10 and len(entities) > 0:
 		ent = entities.pop(0)
 		cmd = "CONFIG_KEYNAME={} ./runOnDF.sh &".format(ent.key.name.strip())
