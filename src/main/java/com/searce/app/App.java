b7a086405c50df48e608f97fa571637a4e29d6fa
Deepak Kar <kar.deepak2011@gmail.com>
2019-05-28 16:05:58 +0530

    Update

--- src/main/java/com/searce/app/App.java
+++ src/main/java/com/searce/app/App.java
@@ -1,7 +1,5 @@
 package com.searce.app;
 
-import java.util.ArrayList;
-
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.coders.KvCoder;
 import org.apache.beam.sdk.coders.StringUtf8Coder;
@@ -9,7 +7,6 @@ import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.jdbc.JdbcIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Count;
-import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.Distinct;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.MapElements;
@@ -19,33 +16,23 @@ import org.apache.beam.sdk.values.PCollection;
 
 
 public class App {
-	static PCollection<KV<String, String>> getRows(Pipeline p, Options options) {
-		ArrayList<String> LINES = new ArrayList<String>();
-		LINES.add("Nothin");
-		return p.apply("Nothin'", Create.of(LINES)).setCoder(StringUtf8Coder.of())
-			.apply("Read Data From Web API", ParDo.of(new GetDataFn()));
-	}
 	
 	@SuppressWarnings("serial")
 	static void run(Options options) {
 	    Pipeline p = Pipeline.create(options);
 	    PCollection<KV<String, String>> rows;
-	
-	    if(options.getURL() == null || options.getURL().isEmpty()) {
-	    	rows = p.apply("Reading Database", 
-	    		JdbcIO.<KV<String, String>>read()
-	    			.withDataSourceConfiguration(
-	    					JdbcIO.DataSourceConfiguration
-	    						.create(options.getDatabaseDriver(), options.getDatabaseConnectionURL())
-	    						.withUsername(options.getDatabaseUsername())
-	    						.withPassword(options.getDatabasePassword())
-	    			)
-	    			.withQuery(options.getDatabseQuery())
-	    			.withRowMapper(new CustomRowMapper())
-	    			.withCoder(KvCoder.of(StringUtf8Coder.of(), StringUtf8Coder.of()))
-	    	);
-	    }
-	    else	rows = getRows(p, options);
+    	rows = p.apply("Reading Database", 
+    		JdbcIO.<KV<String, String>>read()
+    			.withDataSourceConfiguration(
+    					JdbcIO.DataSourceConfiguration
+    						.create(options.getDatabaseDriver(), options.getDatabaseConnectionURL())
+    						.withUsername(options.getDatabaseUsername())
+    						.withPassword(options.getDatabasePassword())
+    			)
+    			.withQuery(options.getDatabseQuery())
+    			.withRowMapper(new CustomRowMapper())
+    			.withCoder(KvCoder.of(StringUtf8Coder.of(), StringUtf8Coder.of()))
+    	);
 	
 	    rows.apply("Extract Data From Rows", ParDo.of(new DoFn<KV<String, String>, String>() {
 	    	@ProcessElement
